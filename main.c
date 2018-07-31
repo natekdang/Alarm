6058b99c1abdfc3419e4a4b458f37f5a57cc228b
natekdang <ndang008@ucr.edu>
2018-07-31 12:00:40 -0700

    README
    Program in C that sends "ON/OFF" through serial port at desired times to alert employees. Nucleo F411RE will read the serial port and turn on the alarm.

---
+++ main.c
@@ -0,0 +1,269 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <time.h>
+#include <Windows.h>
+#include <tchar.h>
+
+//ALARM TIMES-------------------------
+#define START		"080000"
+#define BREAK		"100000"
+#define BREAKOVER	"101500"
+#define LUNCH1		"120000"
+#define LUNCH2		"123000"
+#define LUNCH3		"130000"
+#define LEAVE		"163000"
+
+//GLOBALS-----------------------------
+char array[6];
+int count;
+
+//STATES------------------------------
+enum States {Monitor, SetOn, SetOff} State; 
+
+//HELPER FUNCTIONS--------------------
+void delay(int numSeconds)
+{
+	int i; 
+	int j;
+
+	for (i = 0; i < numSeconds; ++i)
+	{
+		for (j = 0; j < 300500000; ++j);
+	}
+}
+
+int compareTimeArray(char array[])
+{
+	if (strcmp(array, START) == 0)  
+	{
+		return 1; 
+	}
+	else if (strcmp(array, BREAK) == 0) 
+	{
+		return 1; 
+	}
+	else if (strcmp(array, BREAKOVER) == 0)  
+	{
+		return 1; 
+	}
+	else if (strcmp(array, LUNCH1) == 0) 
+	{
+		return 1; 
+	}
+	else if (strcmp(array, LUNCH2) == 0) 
+	{
+		return 1; 
+	}
+	else if (strcmp(array, LUNCH3) == 0)
+	{
+		return 1;  
+	}
+	else if (strcmp(array, LEAVE) == 0)  
+	{
+		return 1;  
+	}
+	else if (strcmp(array, "112400") == 0) //TEST BRANCH
+	{
+		return 1; 
+	}
+	else 
+	{
+		return 0;
+	}
+}
+
+//TICK FUNCTION----------------------
+void TickFct()
+{
+	//time variables
+	time_t curtime; 
+	struct tm *loctime;
+
+	//serial variables
+	HANDLE hComm;
+	char onBuffer[]				= "On\r";
+	char offBuffer[]			= "Off\r";
+	BOOL Status; 
+	DWORD onBytesToWrite;
+	DWORD onBytesWritten		= 0; 
+	DWORD offBytesToWrite; 
+	DWORD offBytesWritten		= 0;
+	DCB dcbParameters			= {0}; 
+	COMMTIMEOUTS timeouts		= {0};
+
+	onBytesToWrite				= sizeof(onBuffer);
+	offBytesToWrite				= sizeof(offBuffer); 
+
+	switch(State)
+	{
+		case Monitor:
+			if (compareTimeArray(array))
+			{
+				count = 0; 
+				printf("ON TEST\r\n");
+
+				//*********OPEN SERIAL PORT
+				hComm = CreateFile(_T("COM3"),							//port name
+									GENERIC_READ | GENERIC_WRITE,		//read/write
+									0,									//no sharing
+									NULL,								//no security
+									OPEN_EXISTING,						//opening existing port only
+									0, //FILE_FLAG_OVERLAPPED,			//overlapped I/O, change to 0 for non overlapped
+									NULL);								//null for comm devices
+				if (hComm == INVALID_HANDLE_VALUE)
+				{
+					printf("Error in opening serial port\n"); 
+				}
+				else 
+				{
+					printf("Serial port opened successfully\n");
+				}
+
+				//************SET PARAMETERS 
+				dcbParameters.DCBlength = sizeof(dcbParameters);
+
+				Status = GetCommState(hComm, &dcbParameters);
+
+				if (Status == FALSE)
+				{
+					printf("Error in GetCommState()");
+				}
+				dcbParameters.BaudRate	= CBR_115200;
+				dcbParameters.ByteSize	= 8;
+				dcbParameters.StopBits	= ONESTOPBIT;
+				dcbParameters.Parity	= NOPARITY;
+
+				Status = SetCommState(hComm, &dcbParameters);  //Configuring the port according to settings in DCB 
+				if (Status == FALSE)
+				{
+					printf("Error in Setting DCB Structure");
+				}
+
+				//************SET TIMEOUTS
+				timeouts.ReadIntervalTimeout			= 50;
+				timeouts.ReadTotalTimeoutConstant		= 50;
+				timeouts.ReadTotalTimeoutMultiplier		= 10;
+				timeouts.WriteTotalTimeoutConstant		= 50;
+				timeouts.WriteTotalTimeoutMultiplier	= 10;
+
+				if (SetCommTimeouts(hComm, &timeouts) == FALSE)
+				{
+					printf("Error in Setting Time Outs");
+				}
+
+				//************WRITE TO SERIAL PORT
+				Status = WriteFile(hComm, 
+								   onBuffer, 
+								   onBytesToWrite,
+								   &onBytesWritten, 
+								   NULL); 
+
+				if (Status == TRUE)
+				{
+					printf("Serial Write Success\n");
+				}
+				else 
+				{
+					printf("Serial Write Failure\n");
+				}
+
+				//**************CLOSE SERIAL PORT
+				CloseHandle(hComm); 
+
+				State = SetOn;
+			}
+			else
+			{
+				State = Monitor;
+			} //outer if else branch
+			break;
+
+		case SetOn:
+			if (count < 15)
+			{
+				delay(1);
+				State = SetOn;
+			}
+			else
+			{
+				State = SetOff;
+				count = 0; 
+			}
+			break;
+
+		case SetOff:
+			State = Monitor; 
+			break;
+	} //end first switch
+
+	switch(State)
+	{
+		case Monitor:
+			curtime = time(NULL); 
+			loctime = localtime(&curtime); 
+			strftime(array, 7, "%H%M%S", loctime);
+			//printf("The clock time is: %s\n", array);
+			delay(1); 
+			break;
+
+		case SetOn:
+			++count;
+			//printf("%i", count);
+			break;
+
+		case SetOff:
+			printf("OFF TEST NO SERIAL YET\r\n");
+
+			//*********OPEN SERIAL PORT
+			/*hComm = CreateFile(_T("COM3"),							//port name
+								GENERIC_READ | GENERIC_WRITE,		//read/write
+								0,									//no sharing
+								NULL,								//no security
+								OPEN_EXISTING,						//opening existing port only
+								0, //FILE_FLAG_OVERLAPPED,			//overlapped I/O, change to 0 for non overlapped
+								NULL);								//null for comm devices
+			if (hComm == INVALID_HANDLE_VALUE)
+			{
+				printf("Error in opening serial port\n"); 
+			}
+			else 
+			{
+				printf("Serial port opened successfully\n");
+			}
+
+
+
+			//************WRITE TO SERIAL PORT
+			Status = WriteFile(hComm, 
+							   offBuffer, 
+							   offBytesToWrite,
+							   &offBytesWritten, 
+							   NULL); 
+
+			if (Status == TRUE)
+			{
+				printf("Serial Write Success\n");
+			}
+			else 
+			{
+				printf("Serial Write Failure\n");
+			}
+
+			//**************CLOSE SERIAL PORT
+			CloseHandle(hComm); */
+			
+			break;
+	} //end second switch
+}
+
+//MAIN----------------------------
+void main( void )
+{
+	State = Monitor; 
+	
+	while(1)
+	{
+		TickFct(); 
+	}
+} 
